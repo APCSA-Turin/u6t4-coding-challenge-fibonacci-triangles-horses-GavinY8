c9d1de47c68041c5ac15d21d0cc0a3b98529a264
GavinY8 <gaviny8@nycstudents.net>
2025-01-08 13:46:45 +0000

    im so hungru i could eat a-

--- src/main/java/com/example/project/Main.java
+++ src/main/java/com/example/project/Main.java
@@ -2,19 +2,9 @@ package com.example.project;
 
 public class Main {
     public static void main(String[] args) {
-        TriangleCollection test = new TriangleCollection(2, 5, 7);
-        System.out.println(test.totalPerimeter());
-
-        Point p1 = new Point(-5,0);
-        Point p2 = new Point(0,7);
-        Point p3 = new Point(5,0);
-
-        Point p4 = new Point(-5,0);
-        Point p5 = new Point(0,7);
-        Point p6 = new Point(4,0);
-
-        Triangle qwe = new Triangle(p1, p2, p3);
-        Triangle wer = new Triangle(p4, p5, p6);
-        System.out.println(qwe.perimeter()+wer.perimeter());
+        HorseBarn barn1 = new HorseBarn(3);
+        barn1.placeHorse(new Horse("Silver",1210), 2);
+        barn1.placeHorse(new Horse("Trigger", 1340), 0);
+        System.out.println(barn1.horseBarnInfo());
     }
 }
